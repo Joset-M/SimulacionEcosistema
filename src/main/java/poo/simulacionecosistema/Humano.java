ef90cf5b4edad6a266a0a0a5e8a75d4e70d69bae
Joset-Muela <josetmuela@gmail.com>
2026-02-01 10:07:39 -0500

    Cola de prioridad

--- src/main/java/poo/simulacionecosistema/Humano.java
+++ src/main/java/poo/simulacionecosistema/Humano.java
@@ -1,8 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo.simulacionecosistema;
+
+/**
+ *
+ * @author Usuario-Asus
+ */
 public class Humano extends Especie {
 
     public Humano(int poblacion) {
-        super("Humano", poblacion, 1); // riesgo bajo por defecto
+        super("Humano", poblacion, TipoEspecie.HUMANO); // riesgo bajo por defecto
     }
+
+
 
     // Ejemplo de comportamiento específico (opcional)
     public void intervenirEcosistema() {
